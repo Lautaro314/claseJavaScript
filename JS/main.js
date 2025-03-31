589b1b5797de4fd32c4df4c507b60de91a7a73b4
lautarofrioni <lauti.tatengue@hotmail.com>
2025-03-31 17:28:37 -0300

    primer pre-entrega

--- JS/main.js
+++ JS/main.js
@@ -1,39 +1,4 @@
 
-/*
-const productos = ["remera oversize" , "zapatillas campus" , "jean mom con rotura" , "chombas"];
-const carritoProductos = [];
-
-
-//funcion para agregar 2 productos de la lista al carrito
-function agregarProductoAlCarrito (producto) {
-
-    const agregar = prompt("agrega el primer producto de esta lista al carrito: 1- Remera Oversize 2- zapatillas campus 3-Jean mom con rotura , 4-chombas");
-
-        
-        if (productos.includes(agregar)) {
-
-        carritoProductos.push(agregar)
-
-        alert ("el producto es " + carritoProductos);
-
-        } else {
-
-            alert ("este producto no existe");
-
-        }
-        
-        console.log(carritoProductos);
-        
-
-    }
-
-agregarProductoAlCarrito();
-*/
-
-//si el usuario agrega un producto desde la lista al carrito, alert : el producto es (...)
-//si el usuario agrega un producto que no existe en la lista, alert : el producto no existe
-
-
 const carritoProducto = [];
 
 const productos = [
@@ -73,7 +38,3 @@ agregarProductoAlCarrito();
 
 
 
-//productos.push({id: 1 , nombre:"remera oversize" , precio: 10000}),
-//productos.push({id: 2 , nombre:"zapatillas campus" , precio: 30000})
-// //productos.push({id: 3 , nombre:"jean mom con rotura" , precio: 22000}),
-//productos.push({id: 4 , nombre:"chombas" , precio: 9000}),
