e5444fb127ee94c9ed874f6dc097694caf43e959
lautarofrioni <lauti.tatengue@hotmail.com>
2025-03-31 17:23:34 -0300

    mejorando el programa

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,5 @@
 
-
+/*
 const productos = ["remera oversize" , "zapatillas campus" , "jean mom con rotura" , "chombas"];
 const carritoProductos = [];
 
@@ -28,7 +28,52 @@ function agregarProductoAlCarrito (producto) {
     }
 
 agregarProductoAlCarrito();
+*/
 
 //si el usuario agrega un producto desde la lista al carrito, alert : el producto es (...)
 //si el usuario agrega un producto que no existe en la lista, alert : el producto no existe
 
+
+const carritoProducto = [];
+
+const productos = [
+    ({id: 1 , nombre:"remera oversize" , precio: 10000}),
+    ({id: 2 , nombre:"zapatillas campus" , precio: 30000}),
+    ({id: 3 , nombre:"jean mom con rotura" , precio: 22000}),
+    ({id: 4 , nombre:"chombas" , precio: 9000}),
+]
+
+
+function agregarProductoAlCarrito (product) {
+
+    const agregar = parseInt(prompt("agrega el primer producto de esta lista al carrito: 1- Remera Oversize 2- zapatillas campus 3-Jean mom con rotura , 4-chombas"));
+
+    const productoAgregado = productos.find(producto => producto.id === agregar)
+
+    if (productoAgregado) {
+        
+    carritoProducto.push(productoAgregado)
+
+    const nombres = productoAgregado.nombre;
+        
+    alert ("el producto agregado es " + nombres)
+    
+    } else {
+    
+    alert("este producto no existe");
+
+    }
+    
+}
+    
+
+
+
+agregarProductoAlCarrito();
+
+
+
+//productos.push({id: 1 , nombre:"remera oversize" , precio: 10000}),
+//productos.push({id: 2 , nombre:"zapatillas campus" , precio: 30000})
+// //productos.push({id: 3 , nombre:"jean mom con rotura" , precio: 22000}),
+//productos.push({id: 4 , nombre:"chombas" , precio: 9000}),
